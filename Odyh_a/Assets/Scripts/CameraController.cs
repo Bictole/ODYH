179a4cc29a8c46f1ba7cd05e72825951f55e5361
alexandre.lemonnier <alexandre.lemonnier@epita.fr>
2019-03-12 10:41:56 +0100

    New version

---
+++ Odyh_a/Assets/Scripts/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    public GameObject followTarget;
+    private Vector3 targetposition;
+    public float moveSpeed;
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var target = followTarget.transform.position;
+        var position = transform.position;
+        targetposition = new Vector3(target.x, target.y, position.z);
+        position = Vector3.Lerp(position, targetposition, moveSpeed * Time.deltaTime);
+        transform.position = position;
+    }
+}
---
+++ Odyh_v/Assets/Scripts/SceneCh.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneCh : MonoBehaviour
+{
+    // Variable -> la scene ou on envoie le personnage
+    public string scene_a_charger;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //On utilise le OnTrigger pour activer cette fonction des que le perso déclenche ce trigger
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            SceneManager.LoadScene(scene_a_charger);
+        }
+    }
+}
