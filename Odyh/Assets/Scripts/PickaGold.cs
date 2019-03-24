f3d9434a155506424865b03e71da7803e9224338
Victor SIMONIN <victor.simonin@epita.fr>
2019-03-24 12:28:34 +0100

    MoneySystem (interface rapide + scripts pour le perso)

---
+++ Odyh/Assets/Scripts/PickaGold.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickaGold : MonoBehaviour
+{
+    //La valeur a ajouter
+    public int goldvalue;
+    //La référence au manager
+    public MoneyManager manager;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        manager = FindObjectOfType<MoneyManager>();    //on cherche le manager 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)    //on utilise ici la fonction associée au Collider sur le coin
+    {
+        if (other.gameObject.name == "Player")
+        {
+            manager.GainMoney(goldvalue);
+            Destroy(gameObject);
+        }
+    }
+}
